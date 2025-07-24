cd137d706d069e5d59e9ba9faa623637b2c8d163
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2025-07-24 13:16:09 +0200

    Fix whitespace

--- trac_ik_lib/src/trac_ik.cpp
+++ trac_ik_lib/src/trac_ik.cpp
@@ -404,7 +404,7 @@ int TRAC_IK::CartToJnt(const KDL::JntArray &q_init, const KDL::Frame &p_in, KDL:
 {
   if (!initialized)
   {
-    RCLCPP_ERROR(nh_->get_logger(), "TRAC-IK was not properly initialized with a valid chain or limits.  IK cannot proceed");
+    RCLCPP_ERROR(nh_->get_logger(), "TRAC-IK was not properly initialized with a valid chain or limits. IK cannot proceed");
     return -1;
   }
 
